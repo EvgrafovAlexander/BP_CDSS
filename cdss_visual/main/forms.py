e62fa0fe93bf6042d361737e0aa1cd2e6413bb40
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2020-10-12 17:43:30 +0500

    upd forms

--- cdss_visual/main/forms.py
+++ cdss_visual/main/forms.py
@@ -1,6 +1,7 @@
 from .models import Patients, Document
 from django.forms import ModelForm, TextInput, FileInput
 
+
 class PatientsForm(ModelForm):
     class Meta:
         model = Patients
@@ -25,19 +26,3 @@ class UploadDocumentForm(ModelForm):
             "document": FileInput(attrs={
                 'class': 'form-data',
             })}
-
-
-class AddPatientForm(ModelForm):
-    class Meta:
-        model = Patients
-        fields = ["first_name", "last_name"]
-        widgets = {
-            "first_name": TextInput(attrs={
-                'class': 'form-control',
-                'value': Patients.first_name,
-                }),
-            "last_name": TextInput(attrs={
-                'class': 'form-control',
-                'value': Patients.last_name,
-                }),
-        }
