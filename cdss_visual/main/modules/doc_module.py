fc9ca2152cd8b283dda88180e7cecbe83a769a71
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2020-10-16 11:16:19 +0500

    upd  doc_module

--- cdss_visual/main/modules/doc_module.py
+++ cdss_visual/main/modules/doc_module.py
@@ -1,49 +1,29 @@
 import os, re, docx
 
-'''
-path = os.getcwd() + '/Patients_data'
-names_of_documents = os.listdir(path)
-
-
-patients_docx = []
-for name_of_document in names_of_documents:
-    full_path = path + '/' + name_of_document
-    patients_docx.append(docx.Document(full_path))
-'''
-
-
-class DocFiles:
-    def __init__(self):
-        self.path = os.getcwd() + '/Patients_data'
-        self.names_of_documents = os.listdir(self.path)
-
-    def read_docs(self):
-        """
-        Выполняет чтение документов
-        :return: список документов
-        """
-        self.docs = []
-        for name_of_document in self.names_of_documents:
-            self.docs.append(docx.Document(self.path + '/' + name_of_document))
-
-
-def parse_doc_file_test():
-    return {"first_name": 'Игорь', "last_name": 'Николаев'}
-
 
 def parse_doc_file(doc):
-    # выражение для поиска ФИО
-    pattern = r"[А-Я][а-я]+\s[А-Я][а-я]+\s[А-Я][а-я]+"
-
+    data = dict()
     document = docx.Document(doc)
+
+    text_data = get_text_data(document)
+    data['text_data'] = text_data
+
+    return data
+
+
+def get_text_data(document):
+    text_data = dict()
+    # выражение для поиска ФИО
+    names_pattern = r"[А-Я][а-я]+\s[А-Я][а-я]+\s[А-Я][а-я]+"
 
     text = ''
     for paragraph in document.paragraphs:
         text += paragraph.text + '\n'
 
-    names = re.findall(pattern, text)[0].split()
-    data = {"first_name": names[1],
-            "last_name": names[0],
-            "middle_name": names[2],
-            }
-    return data
+    names = re.findall(names_pattern, text)[0].split()
+
+    text_data["last_name"] = names[0]
+    text_data["first_name"] = names[1]
+    text_data["middle_name"] = names[2]
+
+    return text_data
