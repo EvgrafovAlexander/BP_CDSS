3918bb11806600041afd316aa6d14f1ac5708f34
EvgrafovAlexander <35798672+EvgrafovAlexander@users.noreply.github.com>
2020-10-21 16:30:37 +0500

    upd doc_module

--- cdss_visual/main/modules/doc_module.py
+++ cdss_visual/main/modules/doc_module.py
@@ -5,8 +5,7 @@ def parse_doc_file(doc):
     data = dict()
     document = docx.Document(doc)
 
-    text_data = get_text_data(document)
-    data['text_data'] = text_data
+    data['text_data'] = get_text_data(document)
 
     return data
 
@@ -25,6 +24,51 @@ def get_text_data(document):
     text_data["last_name"] = names[0]
     text_data["first_name"] = names[1]
     text_data["middle_name"] = names[2]
-    text_data["date_of_birth"] = datetime.datetime.strptime("21.11.1986", '%d.%m.%Y').date()
+    text_data["date_of_birth"] = get_date_between_sections(r'Дата рождения:',
+                                                           r'Дата поступления в стационар:',
+                                                           text)
 
+    text_data["receipt_date"] = get_date_between_sections(r'Дата поступления в стационар',
+                                                          r'Дата выписки из стационара',
+                                                          text)
+
+    text_data["discharge_date"] = get_date_between_sections(r'Дата выписки из стационара',
+                                                            r'Полный диагноз',
+                                                            text)
     return text_data
+
+
+def get_date_between_sections(beg_sec, end_sec, text):
+    # ищем позиции тегов в тексте
+    beg_point = re.search(beg_sec, text)
+    end_point = re.search(end_sec, text)
+    # 27.12.1985
+    matches = re.findall(r'\d\d.\d\d.\d\d\d\d', text[beg_point.end():end_point.start()])
+    if matches:
+        return datetime.datetime.strptime(matches[0], '%d.%m.%Y').date()
+    # 27.12.85
+    matches = re.findall(r'\d\d.\d\d.\d\d', text[beg_point.end():end_point.start()])
+    if matches:
+        day_mon = matches[0][:-2]
+        year = matches[0][-2:]
+        if int(year) > 35:
+            year = '19' + matches[0][-2:]
+        else:
+            year = '20' + matches[0][-2:]
+        return datetime.datetime.strptime(day_mon + year, '%d.%m.%Y').date()
+    # 1985
+    matches = re.findall(r'\d\d\d\d', text[beg_point.end():end_point.start()])
+    if matches:
+        return datetime.datetime.strptime('01.01.' + matches[0], '%d.%m.%Y').date()
+    # 85
+    matches = re.findall(r'\d\d', text[beg_point.end():end_point.start()])
+    if matches:
+        year = matches[0]
+        if int(year) > 35:
+            year = '19' + matches[0]
+        else:
+            year = '20' + matches[0]
+        return datetime.datetime.strptime('01.01.' + year, '%d.%m.%Y').date()
+
+    return datetime.datetime.strptime("01.01.1900", '%d.%m.%Y').date()
+
